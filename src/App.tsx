58c0668c41701036e513d2fd8e5a42e3319a405f
Vasyl Mykhaniv <djvaij@gmail.com>
2023-05-16 14:57:22 +0300

    Add some routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from '@/pages/Home'
 import { About } from '@/pages/About'
+import { Login } from './pages/Login/Login'
+import { ProtectedRoute } from '@/components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
